ca48aec2c60f0d306ff71e357fe10e989d24acd5
Abishek Paudel <abishek.paudel@swifttech.com.np>
2024-10-07 17:23:06 +0545

    style basic modifed

--- past learning components/stylebasic.js
+++ past learning components/stylebasic.js
@@ -3,28 +3,71 @@ import {
     Text,
     ScrollView,
     StatusBar,
+    Button,
+    Modal,
     StyleSheet,
   } from "react-native";
   
+  import { useState } from "react";
+  
   export default function App() {
+    const [isModalVisible, setIsModalVisible] = useState(false);
     return (
       <View style={styleMain.container}>
-        <View style={styleMain.darkMode}>
-          <Text style={styleMain.darkModeText}>Style inheritance</Text>
-        </View>
+        <ScrollView>
+          <View style={styleMain.darkMode}>
+            <Text style={styleMain.darkModeText}>Style inheritance</Text>
+          </View>
   
-        <Text style={[styleMain.boldText, styleMain.darkModeText]}>
-          Now you see me brother
-        </Text>
-        <Text style={styleMain.darkModeText}>For you, For all of us!!</Text>
+          <Text style={[styleMain.boldText, styleMain.darkModeText]}>
+            Now you see me brother
+          </Text>
+          <Text style={styleMain.darkModeText}>For you, For all of us!!</Text>
   
-        <View style={[styleMain.lightblueBg, styleMain.box, styleMain.boxShadow]}>
-          <Text>Coral box</Text>
-        </View>
+          <View
+            style={[styleMain.lightblueBg, styleMain.box, styleMain.boxShadow]}
+          >
+            <Text>Coral box</Text>
+          </View>
   
-        <View style={[styleMain.box, styleMain.lightgreenBg]}>
-          <Text>Lightgreen box</Text>
-        </View>
+          <View style={[styleMain.box, styleMain.lightgreenBg]}>
+            <Text>Lightgreen box</Text>
+          </View>
+  
+          <Modal
+            visible={isModalVisible}
+            animationType="slide"
+            presentationStyle="pageSheet"
+          >
+            <View style={{ flex: 1, backgroundColor: "black", padding: 60 }}>
+              <Text style={{ color: "white" }}>
+                This is the start of new era you dull creature!
+              </Text>
+              <Button
+                title="Next"
+                onPress={() => setIsModalVisible(false)}
+                color="red"
+              ></Button>
+            </View>
+          </Modal>
+  
+          <Modal
+            visible={isModalVisible}
+            animationType="slide"
+            presentationStyle="pageSheet"
+          >
+            <View style={{ flex: 1, backgroundColor: "black", padding: 60 }}>
+              <Text style={{ color: "white" }}>
+                This is the start of new era you dull creature!
+              </Text>
+              <Button
+                title="Next"
+                onPress={() => setIsModalVisible(true)}
+                color="red"
+              ></Button>
+            </View>
+          </Modal>
+        </ScrollView>
       </View>
     );
   }
@@ -72,3 +115,4 @@ import {
       shadowRadius: 4,
     },
   });
+  
