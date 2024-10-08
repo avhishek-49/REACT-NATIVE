b81e1e46e3a77947595577cab549d95d9e6f7696
Abishek Paudel <abishek.paudel@swifttech.com.np>
2024-10-08 10:14:28 +0545

    fixes

--- components/Box.js
+++ components/Box.js
@@ -1,21 +1,21 @@
-import {View,Text, Stylesheet} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
-export default function Box(){
-    
-
-    return (
-        <View style={mainStyle.container}>
-            <Text>Box</Text>
-        </View>
-    )
+export default function Box() {
+  return (
+    <View style={mainStyle.container}>
+      <Text>Box</Text>
+    </View>
+  );
 }
 
-
 const mainStyle = StyleSheet.create({
-
-    container:{
-        height:100,
-        width:100,
-        backgroundColor:"red"
-    }
-})
+  box: {
+    padding: 20,
+    backgroundColor: "red",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
